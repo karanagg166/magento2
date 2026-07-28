fd142db7cb7d39388b6ebb11af6927f166c45b82
karanagg166 <22bcs130@iiitdmj.ac.in>
2026-07-28 15:25:00 +0530

    user story 18 price adjustment

---
+++ app/code/Kunal/PriceAdjustment/Plugin/PriceInterfacePlugin.php
@@ -0,0 +1,40 @@
+<?php
+namespace Kunal\PriceAdjustment\Plugin;
+
+use Magento\Framework\Pricing\Price\PriceInterface;
+
+class PriceInterfacePlugin
+{
+    const ADJUSTMENT_AMOUNT = 1.79;
+
+    /**
+     * @var bool
+     */
+    private static $isCalculating = false;
+
+    /**
+     * Adjust price value returned by pricing models.
+     *
+     * @param PriceInterface $subject
+     * @param float|bool|null $result
+     * @return float|bool|null
+     */
+    public function afterGetValue(PriceInterface $subject, $result)
+    {
+        if (self::$isCalculating) {
+            return $result;
+        }
+        if ($result === null || $result === false) {
+            return $result;
+        }
+
+        self::$isCalculating = true;
+        try {
+            $result = (float)$result + self::ADJUSTMENT_AMOUNT;
+        } finally {
+            self::$isCalculating = false;
+        }
+
+        return $result;
+    }
+}
